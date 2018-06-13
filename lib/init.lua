5acef9659a177d446800e986b60e4613a35eb418
Lucien Greathouse <me@lpghatguy.com>
2018-06-12 17:45:04 -0700

    Introduce simple 'run' API that performs the same operations as TestBootstrap but without a reporter

--- lib/init.lua
+++ lib/init.lua
@@ -1,16 +1,37 @@
+local Expectation = require(script.Expectation)
+local TestBootstrap = require(script.TestBootstrap)
+local TestEnum = require(script.TestEnum)
+local TestPlan = require(script.TestPlan)
+local TestPlanBuilder = require(script.TestPlanBuilder)
+local TestPlanner = require(script.TestPlanner)
+local TestResults = require(script.TestResults)
+local TestRunner = require(script.TestRunner)
+local TestSession = require(script.TestSession)
+local TextReporter = require(script.Reporters.TextReporter)
+
+local function run(testRoot, callback)
+	local modules = TestBootstrap:getModules(testRoot)
+	local plan = TestPlanner.createPlan(modules)
+	local results = TestRunner.runPlan(plan)
+
+	callback(results)
+end
+
 local TestEZ = {
-	Expectation = require(script.Expectation),
-	TestBootstrap = require(script.TestBootstrap),
-	TestEnum = require(script.TestEnum),
-	TestPlan = require(script.TestPlan),
-	TestPlanBuilder = require(script.TestPlanBuilder),
-	TestPlanner = require(script.TestPlanner),
-	TestResults = require(script.TestResults),
-	TestRunner = require(script.TestRunner),
-	TestSession = require(script.TestSession),
+	run = run,
+
+	Expectation = Expectation,
+	TestBootstrap = TestBootstrap,
+	TestEnum = TestEnum,
+	TestPlan = TestPlan,
+	TestPlanBuilder = TestPlanBuilder,
+	TestPlanner = TestPlanner,
+	TestResults = TestResults,
+	TestRunner = TestRunner,
+	TestSession = TestSession,
 
 	Reporters = {
-		TextReporter = require(script.Reporters.TextReporter),
+		TextReporter = TextReporter,
 	},
 }
 
--- tests/api.lua
+++ tests/api.lua
@@ -1,3 +1,4 @@
 return function(TestEZ)
 	assert(typeof(TestEZ) == "table")
+	assert(typeof(TestEZ.run) == "function")
 end

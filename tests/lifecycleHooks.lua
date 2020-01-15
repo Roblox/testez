bd3d2b87970588f63a68cf2f1b762720120c43d6
Benjamin Brimeyer <bbrimeyer@roblox.com>
2020-01-14 17:59:56 -0800

    refactored lifecycle hooks to use runTestPlan helper function

--- tests/lifecycleHooks.lua
+++ tests/lifecycleHooks.lua
@@ -5,66 +5,65 @@ local function insertLifecycleEvent(lifecycleString)
 	table.insert(lifecycleOrder, lifecycleString)
 end
 
-local function lifecycleOrderMatches(array)
-	-- shallow equals between lifecycleOrder and passed array
-	for index, value in ipairs(lifecycleOrder) do
-		if array[index] ~= value then
-			return false
-		end
-	end
-
-	for index, value in ipairs(array) do
-		if lifecycleOrder[index] ~= value then
-			return false
-		end
-	end
-
-	return true
-end
-
-local function arrayToString(array)
-	return table.concat(array, "\n\t")
-end
-
 local function expectLifecycleOrder(array)
+	local function lifecycleOrderMatches()
+		-- shallow equals between lifecycleOrder and passed array
+		for index, value in ipairs(lifecycleOrder) do
+			if array[index] ~= value then
+				return false
+			end
+		end
+
+		for index, value in ipairs(array) do
+			if lifecycleOrder[index] ~= value then
+				return false
+			end
+		end
+
+		return true
+	end
+
 	assert(
-		lifecycleOrderMatches(array),
-		string.format("lifecycle order did not match expected order.\nGot: {\n\t%s\n}", arrayToString(lifecycleOrder))
+		lifecycleOrderMatches(),
+		string.format("lifecycle order did not match expected order.\nGot: {\n\t%s\n}", table.concat(array, "\n\t"))
 	)
+end
+
+local function runTestPlan(testPlan)
+	lifecycleOrder = {}
+	local plan = TestEZ.TestPlanner.createPlan({
+		{
+			method = testPlan,
+			path = {'lifecycleHooksTest'}
+		}
+	})
+
+	return TestEZ.TestRunner.runPlan(plan)
 end
 
 return {
 	["should run lifecycle methods in single-level"] = function()
-		lifecycleOrder = {}
+		local results = runTestPlan(function()
+			beforeAll(function()
+				insertLifecycleEvent("1 - beforeAll")
+			end)
 
-		local plan = TestEZ.TestPlanner.createPlan({
-			{
-			method = function()
-				beforeAll(function()
-					insertLifecycleEvent("1 - beforeAll")
-				end)
+			afterAll(function()
+				insertLifecycleEvent("1 - afterAll")
+			end)
 
-				afterAll(function()
-					insertLifecycleEvent("1 - afterAll")
-				end)
+			beforeEach(function()
+				insertLifecycleEvent("1 - beforeEach")
+			end)
 
-				beforeEach(function()
-					insertLifecycleEvent("1 - beforeEach")
-				end)
+			afterEach(function()
+				insertLifecycleEvent("1 - afterEach")
+			end)
 
-				afterEach(function()
-					insertLifecycleEvent("1 - afterEach")
-				end)
-
-				it("runs root", function()
-					insertLifecycleEvent("1 - test")
-				end)
-			end,
-			path = {'lifecycleHooksTest'}
-		}
-		})
-
-		local results = TestEZ.TestRunner.runPlan(plan)
+			it("runs root", function()
+				insertLifecycleEvent("1 - test")
+			end)
+		end)
 
 		expectLifecycleOrder({
 			"1 - beforeAll",
@@ -77,63 +76,55 @@ return {
 		assert(results.failureCount == 0)
 	end,
 	["should run lifecycle methods in order in nested trees"] = function()
-		lifecycleOrder = {}
-		local plan = TestEZ.TestPlanner.createPlan({
-			{
-			method = function()
+		local results = runTestPlan(function()
+			beforeAll(function()
+				insertLifecycleEvent("1 - beforeAll")
+			end)
+
+			afterAll(function()
+				insertLifecycleEvent("1 - afterAll")
+			end)
+
+			beforeEach(function()
+				insertLifecycleEvent("1 - beforeEach")
+			end)
+
+			afterEach(function()
+				insertLifecycleEvent("1 - afterEach")
+			end)
+
+			it("runs root", function()
+				insertLifecycleEvent("1 - test")
+			end)
+
+			describe("nestedDescribe", function()
 				beforeAll(function()
-					insertLifecycleEvent("1 - beforeAll")
+					insertLifecycleEvent("2 - beforeAll")
 				end)
 
 				afterAll(function()
-					insertLifecycleEvent("1 - afterAll")
+					insertLifecycleEvent("2 - afterAll")
 				end)
 
 				beforeEach(function()
-					insertLifecycleEvent("1 - beforeEach")
+					insertLifecycleEvent("2 - beforeEach")
 				end)
 
 				afterEach(function()
-					insertLifecycleEvent("1 - afterEach")
+					insertLifecycleEvent("2 - afterEach")
 				end)
 
-				it("runs root", function()
-					insertLifecycleEvent("1 - test")
+				it("runs", function()
+					insertLifecycleEvent("2 - test")
 				end)
 
-				describe("nestedDescribe", function()
-					beforeAll(function()
-						insertLifecycleEvent("2 - beforeAll")
-					end)
-
-					afterAll(function()
-						insertLifecycleEvent("2 - afterAll")
-					end)
-
-					beforeEach(function()
-						insertLifecycleEvent("2 - beforeEach")
-					end)
-
-					afterEach(function()
-						insertLifecycleEvent("2 - afterEach")
-					end)
-
-					it("runs", function()
-						insertLifecycleEvent("2 - test")
-					end)
-
-					describe("no tests", function()
-						before(function()
-							insertLifecycleEvent("3 - beforeAll")
-						end)
+				describe("no tests", function()
+					before(function()
+						insertLifecycleEvent("3 - beforeAll")
 					end)
 				end)
-			end,
-			path = {'lifecycleHooksTest'}
-		}
-		})
-
-		local results = TestEZ.TestRunner.runPlan(plan)
+			end)
+		end)
 
 		expectLifecycleOrder({
 			"1 - beforeAll",
@@ -153,37 +144,29 @@ return {
 		assert(results.failureCount == 0)
 	end,
 	["beforeAll should only run once per describe block"] = function()
-		lifecycleOrder = {}
-		local plan = TestEZ.TestPlanner.createPlan({
-			{
-			method = function()
+		local results = runTestPlan(function()
+			beforeAll(function()
+				insertLifecycleEvent("1 - beforeAll")
+			end)
+
+			it("runs 1", function()
+				insertLifecycleEvent("1 - test")
+			end)
+
+			describe("nestedDescribe", function()
 				beforeAll(function()
-					insertLifecycleEvent("1 - beforeAll")
+					insertLifecycleEvent("2 - beforeAll")
 				end)
 
-				it("runs 1", function()
-					insertLifecycleEvent("1 - test")
+				it("runs 2", function()
+					insertLifecycleEvent("2 - test")
 				end)
 
-				describe("nestedDescribe", function()
-					beforeAll(function()
-						insertLifecycleEvent("2 - beforeAll")
-					end)
-
-					it("runs 2", function()
-						insertLifecycleEvent("2 - test")
-					end)
-
-					it("runs 2 again", function()
-						insertLifecycleEvent("2 - test again")
-					end)
+				it("runs 2 again", function()
+					insertLifecycleEvent("2 - test again")
 				end)
-			end,
-			path = {'lifecycleHooksTest'}
-		}
-		})
-
-		local results = TestEZ.TestRunner.runPlan(plan)
+			end)
+		end)
 
 		expectLifecycleOrder({
 			"1 - beforeAll",

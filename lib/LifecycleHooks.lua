2469f65d6a3e9fdd221be5db82519b718edc9d2f
Benjamin Brimeyer <bbrimeyer@roblox.com>
2020-01-15 14:48:33 -0800

    reorganized LifecycleHooks.lua

--- lib/LifecycleHooks.lua
+++ lib/LifecycleHooks.lua
@@ -1,7 +1,6 @@
 local TestEnum = require(script.Parent.TestEnum)
 local Stack = require(script.Parent.Stack)
 
-
 local LifecycleHooks = {}
 LifecycleHooks.__index = LifecycleHooks
 
@@ -13,10 +12,40 @@ function LifecycleHooks.new()
 end
 
 --[[
+	Returns an array of `beforeEach` hooks in FIFO order
+]]
+function LifecycleHooks:getBeforeEachHooks()
+	local key = TestEnum.NodeType.BeforeEach
+	local hooks = {}
+
+	for _, level in ipairs(self._stack.data) do
+		for _, hook in ipairs(level[key]) do
+			table.insert(hooks, hook)
+		end
+	end
+
+	return hooks
+end
+
+--[[
+	Returns an array of `afterEach` hooks in FILO order
+]]
+function LifecycleHooks:getAfterEachHooks()
+	local key = TestEnum.NodeType.AfterEach
+	local hooks = {}
+	for _, level in ipairs(self._stack.data) do
+		for _, hook in ipairs(level[key]) do
+			table.insert(hooks, 1, hook)
+		end
+	end
+
+	return hooks
+end
+
+--[[
 	Pushes uncalled beforeAll and afterAll hooks back up the stack
 ]]
 function LifecycleHooks:popHooks()
-
 	local popped = self._stack:pop()
 
 	local function pushHooksUp(type)
@@ -35,28 +64,17 @@ function LifecycleHooks:popHooks()
 end
 
 function LifecycleHooks:pushHooksFrom(planNode)
-
 	assert(planNode ~= nil)
 
-	local lastTestNodeAtLevel
-
-	for _, childNode in pairs(planNode.children) do
-		if childNode.type == TestEnum.NodeType.It then
-			lastTestNodeAtLevel = childNode
-		end
-	end
-
 	self._stack:push({
-		lastTestNodeAtLevel = lastTestNodeAtLevel,
-		[TestEnum.NodeType.BeforeAll] = self:_getHooksOfTypeIncludingUncalledAtCurrentLevel(planNode.children, TestEnum.NodeType.BeforeAll),
-		[TestEnum.NodeType.AfterAll] = self:_getHooksOfTypeIncludingUncalledAtCurrentLevel(planNode.children, TestEnum.NodeType.AfterAll),
+		[TestEnum.NodeType.BeforeAll] = self:_getBeforeAllHooksUncalledAtCurrentLevel(planNode.children),
+		[TestEnum.NodeType.AfterAll] = self:_getAfterAllHooksUncalledAtCurrentLevel(planNode.children),
 		[TestEnum.NodeType.BeforeEach] = self:_getHooksOfType(planNode.children, TestEnum.NodeType.BeforeEach),
 		[TestEnum.NodeType.AfterEach] = self:_getHooksOfType(planNode.children, TestEnum.NodeType.AfterEach),
 	})
 end
 
 function LifecycleHooks:getPendingBeforeHooks()
-
 	return self:_getAndClearPendingHooks(TestEnum.NodeType.BeforeAll)
 end
 
@@ -68,50 +86,10 @@ function LifecycleHooks:getAfterAllHooks()
 	end
 end
 
-function LifecycleHooks:getHooksInStackOrder(key)
-
-	assert(key ~= nil)
-
-	local hooks = {}
-
-	for _, level in ipairs(self._stack.data) do
-		for _, hook in ipairs(level[key]) do
-			table.insert(hooks, hook)
-		end
-	end
-
-	return hooks
-end
-
-function LifecycleHooks:getHooksInReverseStackOrder(key)
-
-	assert(key ~= nil)
-
-	local hooks = {}
-	for _, level in ipairs(self._stack.data) do
-		for _, hook in ipairs(level[key]) do
-			table.insert(hooks, 1, hook)
-		end
-	end
-
-	return hooks
-end
-
-function LifecycleHooks:getBeforeEachHooks()
-
-	return self:getHooksInStackOrder(TestEnum.NodeType.BeforeEach)
-end
-
-function LifecycleHooks:getAfterEachHooks()
-
-	return self:getHooksInReverseStackOrder(TestEnum.NodeType.AfterEach)
-end
-
 --[[
 	Return any hooks that have not yet been returned for this key and clear those hooks
 ]]
 function LifecycleHooks:_getAndClearPendingHooks(key)
-
 	assert(key ~= nil)
 
 	if self._stack:size() > 0 then
@@ -126,49 +104,61 @@ function LifecycleHooks:_getAndClearPendingHooks(key)
 	else
 		return {}
 	end
-
-end
-
-function LifecycleHooks:_getBackOfStack()
-
-	return self._stack:size() > 0 and self._stack:getBack() or nil
-end
-
-function LifecycleHooks:_getHooksOfType(nodes, type)
-
-	local hooks = {}
-
-	for _, node in pairs(nodes) do
-		if node.type == type then
-			table.insert(hooks, node.callback)
-		end
-	end
-
-	return hooks
 end
 
 --[[
 	Transfers uncalled beforeAll and afterAll hooks down the stack
 ]]
-function LifecycleHooks:_getHooksOfTypeIncludingUncalledAtCurrentLevel(childNodes, type)
+function LifecycleHooks:_getBeforeAllHooksUncalledAtCurrentLevel(childNodes)
+	local hookType = TestEnum.NodeType.BeforeAll
+	local hooks = self:_getHooksOfTypeFromBackOfStack(hookType)
+
+	for _, hook in pairs(self:_getHooksOfType(childNodes, hookType)) do
+		table.insert(hooks, hook)
+	end
+
+	return hooks
+end
+
+function LifecycleHooks:_getAfterAllHooksUncalledAtCurrentLevel(childNodes)
+	local hookType = TestEnum.NodeType.AfterAll
+	local hooks = self:_getHooksOfTypeFromBackOfStack(hookType)
+
+	for _, hook in pairs(self:_getHooksOfType(childNodes, hookType)) do
+		table.insert(hooks, 1, hook)
+	end
+
+	return hooks
+end
+
+function LifecycleHooks:_getHooksOfTypeFromBackOfStack(hookType)
+	assert(hookType, "Expected hookType to be an argument")
+
 	local currentBack = self:_getBackOfStack()
 
 	local hooks = {}
 
 	if currentBack then
-
-		for _, hook in pairs(currentBack[type]) do
+		for _, hook in pairs(currentBack[hookType]) do
 			table.insert(hooks, hook)
 		end
 
-		currentBack[type] = {}
+		currentBack[hookType] = {}
 	end
 
-	for _, hook in pairs(self:_getHooksOfType(childNodes, type)) do
-		if type == TestEnum.NodeType.AfterAll then
-			table.insert(hooks, 1, hook)
-		else
-			table.insert(hooks, hook)
+	return hooks
+end
+
+function LifecycleHooks:_getBackOfStack()
+	return self._stack:size() > 0 and self._stack:getBack() or nil
+end
+
+function LifecycleHooks:_getHooksOfType(nodes, type)
+	local hooks = {}
+
+	for _, node in pairs(nodes) do
+		if node.type == type then
+			table.insert(hooks, node.callback)
 		end
 	end
 

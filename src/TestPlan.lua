6c36a862a24f4b2d858f991f2582a8c8603e2da1
MagiMaster <MagiMaster@users.noreply.github.com>
2020-05-07 13:58:46 -0700

    Collapse TestPlan/TestPlanner/TestPlanBuilder/TestEnvironment

--- src/TestPlan.lua
+++ src/TestPlan.lua
@@ -7,6 +7,111 @@
 
 local TestEnum = require(script.Parent.TestEnum)
 
+local function newEnvironment(currentNode, extraEnvironment)
+	local env = {}
+
+	if extraEnvironment then
+		if type(extraEnvironment) ~= "table" then
+			error(("Bad argument #2 to newEnvironment. Expected table, got %s"):format(
+				typeof(extraEnvironment)), 2)
+		end
+
+		for key, value in pairs(extraEnvironment) do
+			env[key] = value
+		end
+	end
+
+	function env.describeFOCUS(phrase, callback)
+		return env.describe(phrase, callback, TestEnum.NodeModifier.Focus)
+	end
+
+	function env.describeSKIP(phrase, callback)
+		return env.describe(phrase, callback, TestEnum.NodeModifier.Skip)
+	end
+
+	function env.describe(phrase, callback, nodeModifier)
+		local node = currentNode:addChild(phrase, TestEnum.NodeType.Describe, nodeModifier)
+		node.callback = callback
+		node:expand()
+		return node
+	end
+
+	function env.it(phrase, callback, nodeModifier)
+		local node = currentNode:addChild(phrase, TestEnum.NodeType.It, nodeModifier)
+		node.callback = callback
+		return node
+	end
+
+	-- Incrementing counter used to ensure that beforeAll, afterAll, beforeEach, afterEach have unique phrases
+	local lifecyclePhaseId = 0
+
+	local lifecycleHooks = {
+		[TestEnum.NodeType.BeforeAll] = "beforeAll",
+		[TestEnum.NodeType.AfterAll] = "afterAll",
+		[TestEnum.NodeType.BeforeEach] = "beforeEach",
+		[TestEnum.NodeType.AfterEach] = "afterEach"
+	}
+
+	for nodeType, name in pairs(lifecycleHooks) do
+		env[name] = function(callback)
+			local node = currentNode:addChild(name .. "_" .. tostring(lifecyclePhaseId), nodeType)
+			lifecyclePhaseId = lifecyclePhaseId + 1
+
+			node.callback = callback
+			return node
+		end
+	end
+
+	function env.itFOCUS(phrase, callback)
+		return env.it(phrase, callback, TestEnum.NodeModifier.Focus)
+	end
+
+	function env.itSKIP(phrase, callback)
+		return env.it(phrase, callback, TestEnum.NodeModifier.Skip)
+	end
+
+	function env.itFIXME(phrase, callback)
+		local node = env.it(phrase, callback, TestEnum.NodeModifier.Skip)
+		warn("FIXME: broken test", node:getFullName())
+		return node
+	end
+
+	function env.FIXME(optionalMessage)
+		warn("FIXME: broken test", currentNode:getFullName(), optionalMessage or "")
+
+		currentNode.modifier = TestEnum.NodeModifier.Skip
+	end
+
+	function env.FOCUS()
+		currentNode.modifier = TestEnum.NodeModifier.Focus
+	end
+
+	function env.SKIP()
+		currentNode.modifier = TestEnum.NodeModifier.Skip
+	end
+
+	--[[
+		These method is intended to disable the use of xpcall when running
+		nodes contained in the same node that this function is called in.
+		This is because xpcall breaks badly if the method passed yields.
+
+		This function is intended to be hideous and seldom called.
+
+		Once xpcall is able to yield, this function is obsolete.
+	]]
+	function env.HACK_NO_XPCALL()
+		warn("HACK_NO_XPCALL is deprecated. It is now safe to yield in an " ..
+			"xpcall, so this is no longer necessary. It can be safely deleted.")
+	end
+
+	env.fit = env.itFOCUS
+	env.xit = env.itSKIP
+	env.fdescribe = env.describeFOCUS
+	env.xdescribe = env.describeSKIP
+
+	return env
+end
+
 local TestPlan = {}
 
 TestPlan.__index = TestPlan
@@ -14,14 +119,115 @@ TestPlan.__index = TestPlan
 --[[
 	Create a new, empty TestPlan.
 ]]
-function TestPlan.new()
-	local self = {
-		children = {}
+function TestPlan.new(testNamePattern, extraEnvironment)
+	local plan = {
+		children = {},
+		testNamePattern = testNamePattern,
 	}
 
-	setmetatable(self, TestPlan)
+	local Node = {}
+	Node.__index = Node
 
-	return self
+	function Node.new(phrase, nodeType, nodeModifier)
+		nodeModifier = nodeModifier or TestEnum.NodeModifier.None
+
+		local node = {
+			phrase = phrase,
+			type = nodeType,
+			modifier = nodeModifier,
+			children = {},
+			callback = nil,
+		}
+
+		node.environment = newEnvironment(node, extraEnvironment)
+		return setmetatable(node, Node)
+	end
+
+	function Node:addChild(phrase, nodeType, nodeModifier)
+		if testNamePattern and (nodeModifier == nil or nodeModifier == TestEnum.NodeModifier.None) then
+			local name = self:getFullName() .. " " .. phrase
+			if name:match(testNamePattern) then
+				nodeModifier = TestEnum.NodeModifier.Focus
+			else
+				nodeModifier = TestEnum.NodeModifier.Skip
+			end
+		end
+		local child = Node.new(phrase, nodeType, nodeModifier)
+		child.parent = self
+		table.insert(self.children, child)
+		return child
+	end
+
+	function Node:getFullName()
+		if self.parent and self.parent.getFullName then
+			local parentPhrase = self.parent:getFullName()
+			if parentPhrase then
+				return parentPhrase .. " " .. self.phrase
+			end
+		end
+		return self.phrase
+	end
+
+	function Node:expand()
+		local originalEnv = getfenv(self.callback)
+		local callbackEnv = setmetatable({}, { __index = originalEnv })
+		for key, value in pairs(self.environment) do
+			callbackEnv[key] = value
+		end
+		setfenv(self.callback, callbackEnv)
+
+		local success, result = xpcall(self.callback, function(err)
+			return err .. "\n" .. debug.traceback()
+		end)
+
+		if not success then
+			self.loadError = result
+		end
+	end
+
+	plan.Node = Node
+
+	return setmetatable(plan, TestPlan)
+end
+
+function TestPlan:addChild(phrase, nodeType, nodeModifier)
+	if self.testNamePattern and (nodeModifier == nil or nodeModifier == TestEnum.NodeModifier.None) then
+		if phrase:match(self.testNamePattern) then
+			nodeModifier = TestEnum.NodeModifier.Focus
+		else
+			nodeModifier = TestEnum.NodeModifier.Skip
+		end
+	end
+	local child = self.Node.new(phrase, nodeType, nodeModifier)
+	child.parent = self
+	table.insert(self.children, child)
+	return child
+end
+
+--[[
+
+]]
+function TestPlan:addRoot(path, method)
+	local curNode = self
+	for i = #path, 1, -1 do
+		local nextNode = nil
+
+		for _, child in ipairs(curNode.children) do
+			if child.phrase == path[i] then
+				nextNode = child
+				break
+			end
+		end
+
+		if nextNode == nil then
+			nextNode = curNode:addChild(path[i], TestEnum.NodeType.Describe)
+		end
+
+		curNode = nextNode
+	end
+
+	curNode.callback = method
+	curNode:expand()
 end
 
 --[[
@@ -36,34 +242,6 @@ function TestPlan:visitAllNodes(callback, root, level)
 
 		self:visitAllNodes(callback, child, level + 1)
 	end
-end
-
-local function constructNodeFullName(node)
-	if node.parent then
-		local parentPhrase = constructNodeFullName(node.parent)
-		if parentPhrase then
-			return parentPhrase .. " " .. node.phrase
-		end
-	end
-	return node.phrase
-end
-
---[[
-	Creates a new node that would be suitable to insert into the TestPlan.
-]]
-function TestPlan.createNode(phrase, nodeType, nodeModifier)
-	nodeModifier = nodeModifier or TestEnum.NodeModifier.None
-
-	local node = {
-		phrase = phrase,
-		type = nodeType,
-		modifier = nodeModifier,
-		children = {},
-		callback = nil,
-		getFullName = constructNodeFullName
-	}
-
-	return node
 end
 
 --[[
--- src/TestPlanner.lua
+++ src/TestPlanner.lua
@@ -3,44 +3,9 @@
 
 	Uses a TestPlanBuilder to keep track of the state of the tree being built.
 ]]
-
-local TestEnum = require(script.Parent.TestEnum)
-local TestPlanBuilder = require(script.Parent.TestPlanBuilder)
-local TestEnvironment = require(script.Parent.TestEnvironment)
+local TestPlan = require(script.Parent.TestPlan)
 
 local TestPlanner = {}
-
-local function buildPlan(builder, module, env)
-	local currentEnv = getfenv(module.method)
-
-	for key, value in pairs(env) do
-		currentEnv[key] = value
-	end
-
-	local nodeCount = #module.path
-
-	-- Dive into auto-named nodes for this module
-	for i = nodeCount, 1, -1 do
-		local name = module.path[i]
-		builder:pushNode(name, TestEnum.NodeType.Describe)
-	end
-
-	local ok, err = xpcall(module.method, function(err)
-		return err .. "\n" .. debug.traceback()
-	end)
-
-	-- This is an error outside of any describe/it blocks.
-	-- We attach it to the node we generate automatically per-file.
-	if not ok then
-		local node = builder:getCurrentNode()
-		node.loadError = err
-	end
-
-	-- Back out of auto-named nodes
-	for _ = 1, nodeCount do
-		builder:popNode()
-	end
-end
 
 --[[
 	Create a new TestPlan from a list of specification functions.
@@ -49,15 +14,13 @@ end
 	variants), which will be turned into a test plan to be executed.
 ]]
 function TestPlanner.createPlan(specFunctions, testNamePattern, extraEnvironment)
-	local builder = TestPlanBuilder.new()
-	builder.testNamePattern = testNamePattern
-	local env = TestEnvironment.new(builder, extraEnvironment)
+	local plan = TestPlan.new(testNamePattern, extraEnvironment)
 
 	for _, module in ipairs(specFunctions) do
-		buildPlan(builder, module, env)
+		plan:addRoot(module.path, module.method)
 	end
 
-	return builder:finalize()
+	return plan
 end
 
 return TestPlanner

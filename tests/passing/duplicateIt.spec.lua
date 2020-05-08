5b18bc1ea7f20ecf7f693ffd45a94aa9eee3a5be
MagiMaster <MagiMaster@users.noreply.github.com>
2020-05-08 14:36:17 -0700

    Document current behavior of duplicate it blocks

---
+++ tests/passing/duplicateIt.spec.lua
@@ -0,0 +1,16 @@
+return function()
+	describe("multiple it blocks with the same description", function()
+		it("only the last runs", function()
+			error("first")
+		end)
+		it("only the last runs", function()
+			error("second")
+		end)
+		it("only the last runs", function()
+			error("third")
+		end)
+		it("only the last runs", function()
+			-- This is the only one that will run
+		end)
+	end)
+end

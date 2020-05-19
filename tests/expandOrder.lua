5ae438d499b4df09c4ab251729dbcfe6f309ae05
MagiMaster <MagiMaster@users.noreply.github.com>
2020-05-18 18:23:11 -0700

    Another test to make sure init.spec and afterAll work together

--- tests/expandOrder.lua
+++ tests/expandOrder.lua
@@ -22,4 +22,50 @@ return {
 		local results = TestEZ.TestRunner.runPlan(plan)
 		assert(#results.errors == 0, "init test failed: " .. tostring(results.errors[1]))
 	end,
+	["init.spec.lua afterAll can correctly undo changes"] = function()
+		local initialized = false
+
+		local plan = TestEZ.TestPlanner.createPlan({
+			{
+				method = function()
+					assert(not initialized, "initialized was true in foo/a.spec")
+				end,
+				path = {'a.spec', 'foo'}
+			},
+			{
+				method = function()
+					assert(initialized, "initialized was false in foo/bar/b.spec")
+				end,
+				path = {'b.spec', 'bar', 'foo'}
+			},
+			{
+				method = function()
+					initialized = true
+
+					-- luacheck: globals afterAll
+					afterAll(function()
+						print("!!!!!!!!!!!!!")
+						initialized = false
+					end)
+				end,
+				path = {'bar', 'foo'}
+			},
+			{
+				method = function()
+					assert(initialized, "initialized was false in foo/bar/c.spec")
+				end,
+				path = {'c.spec', 'bar', 'foo'}
+			},
+			{
+				method = function()
+					assert(not initialized, "initialized was true in foo/d.spec")
+				end,
+				path = {'d.spec', 'foo'}
+			},
+		})
+
+		local results = TestEZ.TestRunner.runPlan(plan)
+		assert(#results.errors == 0, "init test failed:\n" ..
+			table.concat(results.errors, "\n"))
+	end,
 }

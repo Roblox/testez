0c75f3cddd436cae5ff5f3b8276b4344dd897d16
MagiMaster <MagiMaster@users.noreply.github.com>
2020-06-02 17:51:56 -0700

    Add Context object.

---
+++ src/Context.lua
@@ -0,0 +1,22 @@
+local Context = {}
+
+function Context.new(parent)
+	local meta = {}
+	local index = {}
+	meta.__index = index
+
+	if parent then
+		for key, value in pairs(getmetatable(parent).__index) do
+			index[key] = value
+		end
+	end
+
+	function meta.__newindex(_obj, key, value)
+		assert(index[key] == nil, string.format("Cannot reassign %s in context", tostring(key)))
+		index[key] = value
+	end
+
+	return setmetatable({}, meta)
+end
+
+return Context
